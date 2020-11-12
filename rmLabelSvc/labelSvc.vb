fd7a643d34fd8fb13ca9c30ed1834520cece3204
Simon Barnett <simonbarnett@emerge-it.co.uk>
2020-11-12 15:59:04 +0000

    Initial Commit

---
+++ rmLabelSvc/labelSvc.vb
@@ -0,0 +1,78 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports System.Data.SqlClient
+
+Public Class labelSvc : Inherits myService
+
+    Private cnstring As String = "Integrated Security=true;Initial Catalog=b060413;Server=LOCALHOST\PRI"
+
+#Region "Override Service Methods"
+
+    Overrides Sub svcStart(ByVal args As Dictionary(Of String, String))
+
+        Log("Starting...")
+        With New Thread(AddressOf hinit)
+            .Start()
+        End With
+
+    End Sub
+
+#End Region
+
+#Region "Threads"
+
+    Private Sub hinit()
+
+        Do
+            Try
+                Using cn As New SqlConnection(cnstring)
+                    cn.Open()
+                    Using cmd As New SqlCommand( _
+                        "SELECT DOC from dbo.ZROD_PRINTABLELABELS() ", _
+                        cn _
+                    )
+                        Using reader As SqlDataReader = cmd.ExecuteReader()
+                            While reader.Read()
+                                Print(reader(0))
+
+                            End While
+
+                        End Using
+
+                    End Using
+
+                End Using
+
+            Catch ex As Exception
+                Log(ex.Message)
+
+            Finally
+                Thread.Sleep(2000)
+
+            End Try
+
+        Loop
+
+    End Sub
+
+    Private Sub Print(ByVal id As Integer)
+
+        Using cn As New SqlConnection(cnstring)
+            cn.Open()
+            Using print As New SqlCommand( _
+                String.Format( _
+                    "exec dbo.sp_rmPrint {0}", _
+                    id.ToString _
+                ) _
+                , cn _
+            )
+                print.ExecuteNonQuery()
+
+            End Using
+        End Using
+
+    End Sub
+
+#End Region
+
+End Class
---
+++ rmLabelSvc/myService.vb
@@ -0,0 +1,167 @@
+﻿Imports System.IO
+
+Public MustInherit Class myService : Inherits System.ServiceProcess.ServiceBase
+
+#Region "Start argument variables"
+
+    Private Enum eMode
+        Switch
+        Param
+    End Enum
+
+    Private _StartArg As New Dictionary(Of String, String)
+    Public ReadOnly Property StartArg() As Dictionary(Of String, String)
+        Get
+            Return _StartArg
+        End Get
+    End Property
+
+#End Region
+
+#Region "Start"
+
+    Public Overridable Sub svcStart(ByVal args As Dictionary(Of String, String))
+
+    End Sub
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+
+        With _StartArg
+            If args.Length > 0 Then
+
+                Dim i As Integer = 0
+                Dim m As eMode = eMode.Switch
+                Dim thisSwitch As String = ""
+
+                Do
+                    Select Case args(i).Substring(0, 1)
+                        Case "-", "/"
+                            .Add(args(i).Substring(1), "")
+                            thisSwitch = args(i).Substring(1)
+                            m = eMode.Param
+                        Case Else
+                            Select Case m
+                                Case eMode.Param
+                                    .Item(thisSwitch) = args(i)
+                                    thisSwitch = ""
+                                    m = eMode.Switch
+
+                                Case eMode.Switch
+                                    .Add(args(i), "")
+
+                            End Select
+
+                    End Select
+                    i += 1
+                Loop Until i = args.Count
+
+            End If
+
+        End With
+
+        If Not StartArg.Keys.Contains("debug") Then
+            svcStart(StartArg)
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "Pause"
+
+    Public Overridable Sub svcPause()
+
+    End Sub
+
+    Protected Overrides Sub OnPause()
+        svcPause()
+
+    End Sub
+
+#End Region
+
+#Region "Continue"
+
+    Public Overridable Sub svcContinue()
+
+    End Sub
+
+    Protected Overrides Sub OnContinue()
+        If StartArg.Keys.Contains("debug") Then
+            svcStart(StartArg)
+        Else
+            svcContinue()
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "Stop"
+
+    Public Overridable Sub svcStop()
+
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        svcStop()
+
+    End Sub
+
+#End Region
+
+#Region "Logging"
+
+    Private Function LogFolder() As DirectoryInfo
+        Return New DirectoryInfo( _
+            Path.Combine( _
+                Environment.GetEnvironmentVariable("SystemRoot"), _
+                String.Format( _
+                    "logs\{0}\{1}", _
+                    ServiceName, _
+                    Now.ToString("yyyy-MM") _
+                ) _
+            ) _
+        )
+
+    End Function
+
+    Private Function currentlog() As FileInfo
+        With LogFolder()
+            If Not .Exists Then .Create()
+            Return New FileInfo( _
+                Path.Combine( _
+                    .FullName, _
+                    String.Format( _
+                        "{0}.txt", _
+                        Now.ToString("yyMMdd") _
+                    ) _
+                ) _
+            )
+
+        End With
+
+    End Function
+
+    Public Sub Log(ByVal str, ByVal ParamArray args())
+        Debug.Print("{0}> {1}", Format(Now, "hh:mm:ss"), String.Format(str, args))
+        Dim done As Boolean = False
+        While Not done
+            Try
+                Using log As New StreamWriter(currentlog.FullName, True)
+                    log.WriteLine("{0}> {1}", Format(Now, "hh:mm:ss"), String.Format(str, args))
+                End Using
+                done = True
+
+            Catch ex As Exception
+                Threading.Thread.Sleep(500)
+
+            End Try
+
+        End While
+    End Sub
+
+#End Region
+
+
+End Class
